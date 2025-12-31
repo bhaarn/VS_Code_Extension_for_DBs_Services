facc3fb45bea1068d96868af316fab2b465ff152
bhaarn <92562462+bhaarneval@users.noreply.github.com>
2025-12-31 20:30:33 +0530

    Fix: Include native module dependencies in VSIX package
    - Added bindings and file-uri-to-path to .vscodeignore exceptions
    - Externalized all database drivers in webpack config (pg, mysql2, mongodb, neo4j-driver, ioredis, etc.)
    - Prevents webpack minification from breaking native module constructors
    - Package size: 142 files, 10.05MB (includes all required native binaries)
    - Resolves sqlite3 'Cannot find module bindings' and 'i is not a constructor' errors

--- webpack.config.js
+++ webpack.config.js
@@ -22,6 +22,20 @@ const config = {
     { sqlite3: 'commonjs sqlite3' },
     { 'better-sqlite3': 'commonjs better-sqlite3' },
     { 'cpu-features': 'commonjs cpu-features' },
+    { pg: 'commonjs pg' },
+    { 'pg-native': 'commonjs pg-native' },
+    { mysql2: 'commonjs mysql2' },
+    { mongodb: 'commonjs mongodb' },
+    { 'neo4j-driver': 'commonjs neo4j-driver' },
+    { ioredis: 'commonjs ioredis' },
+    { bullmq: 'commonjs bullmq' },
+    { amqplib: 'commonjs amqplib' },
+    { kafkajs: 'commonjs kafkajs' },
+    { '@elastic/elasticsearch': 'commonjs @elastic/elasticsearch' },
+    { 'ssh2': 'commonjs ssh2' },
+    { dockerode: 'commonjs dockerode' },
+    { 'ftp': 'commonjs ftp' },
+    { 'ssh2-sftp-client': 'commonjs ssh2-sftp-client' },
     function ({ request }, callback) {
       if (/\.node$/.test(request)) {
         return callback(null, 'commonjs ' + request);
